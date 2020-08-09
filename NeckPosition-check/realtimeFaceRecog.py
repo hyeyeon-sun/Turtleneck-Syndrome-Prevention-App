3f3d2b5e496466b99956e250393f69ede547572f
hyeyeon-sun <dlgpdus0416@naver.com>
2020-08-10 00:45:51 +0900

    putText error and mouth recognize error resolution

--- NeckPosition-check/realtimeFaceRecog.py
+++ NeckPosition-check/realtimeFaceRecog.py
@@ -5,10 +5,19 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 leye_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
 mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
-initExist = False
 video_capture = cv2.VideoCapture(0)
 t = 0
 DFR = 0
+initExist = False
+
+def eyeInverter(ex,ey,ew,eh,lx,ly,lw,lh):
+    if lx<ex:
+        cv2.rectangle(roi_color,(lx,ly),(ex+ew,ly+lh),(0,0,0),2)
+        eyelength = ex+ew-lx
+    else:
+        cv2.rectangle(roi_color,(ex,ey),(lx+lw,ly+lh),(0,0,0),2)
+        eyelength = lx+lw-ex
+    return eyelength
 
 while True:
     ret, frame = video_capture.read()
@@ -39,6 +48,7 @@ while True:
         minNeighbors = 5, minSize = (27,30),
         flags = cv2.CASCADE_SCALE_IMAGE
         )
+
         ok = True
         try:
             ex,ey,ew,eh = reye[0]
@@ -52,21 +62,11 @@ while True:
             initExist = True
             preLeye = leye[0]
             preReye = reye[0]
-            if lx<ex:
-                cv2.rectangle(roi_color,(lx,ly),(ex+ew,ly+lh),(0,0,0),2)
-                eyelength = ex+ew-lx
-            else:
-                cv2.rectangle(roi_color,(ex,ey),(lx+lw,ly+lh),(0,0,0),2)
-                eyelength = lx+lw-ex
+            eyelength = eyeInverter(ex,ey,ew,eh,lx,ly,lw,lh)
         elif initExist == True:
             ex,ey,ew,eh = preReye
             lx,ly,lw,lh = preLeye
-            if lx<ex:
-                cv2.rectangle(roi_color,(lx,ly),(ex+ew,ly+lh),(0,0,0),2)
-                eyelength = ex+ew-lx
-            else:
-                cv2.rectangle(roi_color,(ex,ey),(lx+lw,ly+lh),(0,0,0),2)
-                eyelength = lx+lw-ex
+            eyelength = eyeInverter(ex,ey,ew,eh,lx,ly,lw,lh)
         elif initExist == False:
             pass
 
@@ -76,29 +76,30 @@ while True:
         minNeighbors = 3, minSize = (30,30),
         flags = cv2.CASCADE_SCALE_IMAGE
         )
-        mins = np.apply_along_axis(lambda a: np.argmax(a), 0, mouth)
-        mx,my,mw,mh = mouth[mins[1]]
+        try:
+            mins = np.apply_along_axis(lambda a: np.argmax(a), 0, mouth)
+        except:
+            mins = np.apply_along_axis(lambda a: np.argmax(a), 0, premouth)
+            mx,my,mw,mh = premouth[mins[1]]
+        else:
+            mx,my,mw,mh = mouth[mins[1]]
+            premouth = mouth
 
         cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,0,255),2)
         mouthlength = mw
-        t+=1
-        if t<15:
-            cv2.putText(roi_color, "Please keep 0 degrees", (0,100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
-        elif t== 15:
-            DFR = eyelength/mouthlength
+    t+=1
+
+    if t<15:
+        cv2.putText(frame, "Please keep 0 degrees", (30,120), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0))
+    elif t== 15:
+        DFR = eyelength/mouthlength
+    else:
         CFR = eyelength/mouthlength
         faceAngle = (CFR-DFR)/0.05
-        try:
-            cv2.putText(roi_color, faceAngle, (0,100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
-        except:
-            pass
-        print(faceAngle)
+        faceAngle = "faceAngle : " + str(round(faceAngle,3))
+        cv2.putText(frame, faceAngle, (30,120), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0))
 
     cv2.imshow('Video', frame)
-
-
-
-    
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
